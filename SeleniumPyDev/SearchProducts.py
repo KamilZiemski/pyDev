3df042cd9f391eafe3a0d8f3e525ee6b2a8817f9
KamilZiemski <kamil.ziemski@gmail.com>
2015-10-27 22:53:49 +0100

    agasamosiepisze

--- SeleniumPyDev/SearchProducts.py
+++ SeleniumPyDev/SearchProducts.py
@@ -20,13 +20,13 @@ class SearchProducts(unittest.TestCase):
         desired_caps['platform'] = self.PLATFORM
         desired_caps['browserName'] = self.BROWSER
         
-        #sauce_string = self.SAUCE_USERNAME + ':' + self.SUACE_KEY
+        sauce_string = self.SAUCE_USERNAME + ':' + self.SUACE_KEY
 
-        #self.driver = \
-        #    webdriver.Remote('http://' + sauce_string + '@ondemand.saucelabs.com:80/wd/hub', desired_caps)
-            
         self.driver = \
-            webdriver.Remote('http://192.168.0.3:4444/wd/hub',desired_caps)
+            webdriver.Remote('http://' + sauce_string + '@ondemand.saucelabs.com:80/wd/hub', desired_caps)
+            
+        #self.driver = \
+        #    webdriver.Remote('http://192.168.0.3:4444/wd/hub',desired_caps)
         #self.driver = webdriver.Firefox()
         self.driver.get('http://demo.magentocommerce.com/')
         self.driver.implicitly_wait(30)
